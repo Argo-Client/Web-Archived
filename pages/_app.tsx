acebdec9e195712268a9feb7b7e6945db09e56b0
Netfloex <netfloex@gmail.com>
2021-02-01 20:19:21 +0100

    next-themes darkmode, SCSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import "tailwindcss/tailwind.css";
+import "../styles/globals.scss";
 
-function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { ThemeProvider } from "next-themes";
 
-export default App
+const App = ({ Component, pageProps }) => (
+	<ThemeProvider defaultTheme="dark" attribute="class">
+		<Component {...pageProps} />
+	</ThemeProvider>
+);
+
+export default App;
