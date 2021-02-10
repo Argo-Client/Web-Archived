022ab306664a5837698dcdda6f194a3055211dbd
Guusvanmeerveld <guusvanmeerveld@live.nl>
2021-02-10 12:38:18 +0000

    Responsive en betere 404 pagina

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,16 @@
 import Page from "@components/Page";
+
+import { VscSearchStop } from "react-icons/vsc";
+
 export default function NotFound() {
 	return (
-		<Page title="404. Not Found" description="Deze pagina bestaat niet.">
-			<div className="h-screen w-screen flex justify-center items-center text-6xl">404 Not found</div>
+		<Page title="Argo - Error 404" description="Deze pagina bestaat niet.">
+			<div className="h-screen mx-auto container flex justify-center items-center">
+				<div>
+					<VscSearchStop className="not-found text-gray-300 w-1/3 mx-auto h-1/2 mb-6" />
+					<div className="text-3xl md:text-6xl">Pagina niet gevonden.</div>
+				</div>
+			</div>
 		</Page>
 	);
 }
